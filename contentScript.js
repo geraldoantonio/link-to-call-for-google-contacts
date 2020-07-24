77b4a80ce8b5b9982b4fa5ee8ac9564b8e20a86a
Geraldo Junior <junior.bimbo@gmail.com>
2020-07-24 18:18:57 -0300

    refactor content script

--- contentScript.js
+++ contentScript.js
@@ -1,44 +1,42 @@
 'use strict';
 
-let add_call_button = true;
-
-
-const imagePhoneCall = (title='phone call') => {
-  let phoneIcon = document.createElement('img');
-  let iconUrl = chrome.runtime.getURL('images/button-phone.ico');
-  phoneIcon.src = iconUrl;
-  phoneIcon.title = title;
-  phoneIcon.style.width='24px'; 
-  phoneIcon.style.height='24px'; 
-
-  return phoneIcon;
-}
+let addCallButtonToElement = true;
 
 const isRenderDialog = () => !!(document.querySelector('[role="dialog"]'));
 
 const addLinkPhone = () => {
-  let dialog = document.querySelector('[role="dialog"]');
-  let phoneNumbers = dialog.querySelectorAll('[dir="ltr"]');
+  const elements = document.querySelectorAll('span.OCiUOb');
 
-  phoneNumbers.forEach( phoneNumberElement => {
-    let phoneNumber = phoneNumberElement.innerText;
+  elements.forEach(element=>{
+    let phoneNumber = element.parentElement.querySelector('[dir=ltr]').innerText;
+
     let button = document.createElement('button');
-    button.classList = 'VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ paacgc';
-    
+        button.classList = 'VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ paacgc';
+
     let linkButton = document.createElement('a');
-    linkButton.href = `tel:${phoneNumber}`;
-    linkButton.appendChild(imagePhoneCall(phoneNumber));
+        linkButton.href = `tel:${phoneNumber}`;
+        linkButton.appendChild(imagePhoneCall(phoneNumber));
 
-    button.appendChild(linkButton);
+        button.appendChild(linkButton);
 
-    let wrapper = phoneNumberElement.parentElement;
-    if (!!wrapper) wrapper.querySelector('.OCiUOb').appendChild(button);
+    element.appendChild(button);
   });
 
-  add_call_button = false;
+  addCallButtonToElement = false;
+}
+
+const imagePhoneCall = (title='phone call') => {
+  let iconUrl = chrome.runtime.getURL('images/button-phone.ico');
+  let phoneIcon = document.createElement('img');
+      phoneIcon.src = iconUrl;
+      phoneIcon.title = title;
+      phoneIcon.style.width='24px'; 
+      phoneIcon.style.height='24px'; 
+
+  return phoneIcon;
 }
 
 setInterval(() => {
-  if (isRenderDialog() && add_call_button) addLinkPhone();
-  if (!isRenderDialog()) add_call_button = true;
+  if (isRenderDialog() && addCallButtonToElement) addLinkPhone();
+  if (!isRenderDialog()) addCallButtonToElement = true;
 }, 1000);
